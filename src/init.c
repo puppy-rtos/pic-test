2c4a174f6b98b2006bb748435799d9af6f62f565
guozhanxin <guozhanxin@rt-thread.com>
2023-03-08 13:07:18 +0800

    first

---
+++ src/init.c
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2022, The Puppy RTOS Authors
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdint.h>
+
+int main(void)
+{
+
+    while(1)
+    {
+
+    }
+    return 0;
+}
