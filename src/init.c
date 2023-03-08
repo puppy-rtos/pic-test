0ae0ab77bfd63ad697244d22c1abf290626d5ace
guozhanxin <guozhanxin@rt-thread.com>
2023-03-08 23:09:51 +0800

    update

--- src/init.c
+++ src/init.c
@@ -6,8 +6,30 @@
 
 #include <stdint.h>
 
+uint32_t g_a, g_b, g_c;
+uint32_t *g_a_p, *g_b_p, *g_c_p;
+
+uint32_t add(uint32_t a, uint32_t b)
+{
+    return a + b;
+}
+typedef uint32_t (*fun_ptr)(uint32_t a, uint32_t b);
+
+fun_ptr add_func_p;
+
 int main(void)
 {
+    uint32_t l_a, l_b, l_c;
+    uint32_t *l_a_p, *l_b_p, *l_c_p;
+    g_a = 10;
+    g_b = 20;
+    g_a_p = &g_a;
+    g_b_p = &g_b;
+
+    add_func_p = add;
+    g_c = add_func_p(*g_a_p, *g_b_p);
+
+    l_c = add(*g_a_p, *g_b_p);
 
     while(1)
     {
--- xmake.lua
+++ xmake.lua
@@ -3,13 +3,14 @@ includes("toolchains/*.lua")
 
 add_rules("mode.debug", "mode.release")
 
-target("kernel")
+target("kernel-mom")
     add_files("src/*.c")
-    add_includedirs("include")
+    -- add_includedirs("include")
     -- if is_plat("gcc") then
         set_toolchains("arm-none-eabi")
         set_extension(".elf")
         set_arch("cortex-m4")
-        add_links("c", "m", "nosys");
+        add_cflags("-fPIC -fPIE -mno-pic-data-is-text-relative");
+        add_links("nosys");
         add_ldflags(' -T link.lds')
     -- end
